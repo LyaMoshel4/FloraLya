a2c8f1262b9feeb437ed16e2e95b1e1a4a894bdf
lyaUser <User@WX1129734>
2026-01-14 17:01:02 +0200

    more flowers

--- Migrations/20260114141905_UpdateBouquetFields.Designer.cs
+++ Migrations/20260114141905_UpdateBouquetFields.Designer.cs
@@ -3,6 +3,7 @@ using LyaShop.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace LyaShop.Migrations
 {
     [DbContext(typeof(LyaFlowerShopContext))]
-    partial class LyaFlowerShopContextModelSnapshot : ModelSnapshot
+    [Migration("20260114141905_UpdateBouquetFields")]
+    partial class UpdateBouquetFields
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
